5a86e1489f13dfd349045cf004eb86ca47613986
martaprosniak <martaprosniak@gmail.com>
2019-03-11 20:41:38 +0100

    js file

---
+++ www/js/ents.js
@@ -0,0 +1,19 @@
+var ents = [
+{
+	"age" = "120",
+	"species" = "oak",
+	"plant year" = "1899",
+	"mobile" = "yes",
+	"bird's nests": [
+		"species" = "owl",
+		"count" = 4;
+	],
+	
+	
+	
+	
+}
+
+
+
+]
